0d6ff587161d34b1bddbd73b9a57e25fb715ad95
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-21 20:56:57 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,32 @@
+const a = 'alim uddin'
+const b = 32;
+const c = true;
+
+// null
+// undefined
+// primitive 
+// object
+
+const nums = [ 23, 34, 45, 22];
+// console.log(typeof nums);
+// Array.isArray(nums);
+
+function triple( x, y, z){
+    x.age = 8888;
+    // y = 222;
+    // z = 333;
+}
+
+// console.log(typeof triple);
+// console.log(triple.length);
+
+const num1 = 3;
+const num2 = 5;
+const num3 = 7;
+
+triple ( num1, num2, num3);
+const myObj = { name: 'kuddus', age: 17};
+console.log(myObj);
+
+// console.log(num1, num2, num3);
+// console.log(typeof null); 
