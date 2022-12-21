b77bfc289b947cf7e9c856429e9f214cc86625c0
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-21 15:19:18 +0600

    call back

--- callback.js
+++ callback.js
@@ -7,3 +7,25 @@ function welcomeMessage (name, greetHandler){
 function greetMorning(name){
     console.log('Good Morning', name);
 }
+
+function greetAfternoon(name) {
+    console.log('Good afternoon', name);
+}
+
+function greetEvening(name){
+    console.log('Good Evening', name);
+}
+
+welcomeMessage('Tom Hanks', greetMorning);
+welcomeMessage('Sakib Hanks', greetAfternoon);
+welcomeMessage('Bappa raj', greetEvening);
+
+function handleClick (){
+    console.log('button is clicked');
+}
+
+document.getElementById('my-btn').addEventListener('click',handleClick)
+
+document.getElementById('btn').addEventListener('click', function(){
+    console.log('button is clicked');
+})
